c4ddbd306a35024b70c243a49abadfa29d10f9e0
fruzie99 <vmadhurisha99@gmail.com>
2025-12-19 22:17:54 +0530

    React UseEffect Assignment added

---
+++ ReactUseEffect/src/App.jsx
@@ -0,0 +1,14 @@
+import UserData from "./components/UserData";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <UserData />
+      <hr />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
