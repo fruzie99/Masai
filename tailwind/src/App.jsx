4355b218dfc9651287ce0291a254f43d37f36c2d
fruzie99 <vmadhurisha99@gmail.com>
2026-01-04 23:11:46 +0530

    tailwind integration

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working 
+      </h1>
+
+      <button className="mt-4 px-4 py-2 bg-gray-200 rounded hover:bg-gray-300 active:bg-gray-400">
+        Button style
+      </button>
+
+      <h1 className="text-5xl font-bold text-green-600">
+  Tailwind is working 
+</h1>
+
+    </div>
+  );
+}
+
+export default App;
